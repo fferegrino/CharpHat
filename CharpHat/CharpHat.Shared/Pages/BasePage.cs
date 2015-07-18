947263f3c319b83bc1da8bb30599724430db031c
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-07-18 17:47:01 -0500

    Bases laid off based on pierceboggan's Moments app

---
+++ CharpHat/CharpHat.Shared/Pages/BasePage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace CharpHat.Pages
+{
+	public class BasePage : ContentPage
+	{
+        public ObservableCollection<ToolbarItem> ToolbarItems { get; set; }
+		public BasePage ()
+		{
+            ToolbarItems = new ObservableCollection<ToolbarItem>();
+		}
+	}
+}
